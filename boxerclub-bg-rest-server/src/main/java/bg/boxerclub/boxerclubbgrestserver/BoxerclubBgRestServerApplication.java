98b155eac35604b78f122d66ed0fc4aa4535cd4b
magdalenavelikova <magibox@yahoo.com>
2023-09-11 11:19:53 +0300

    client folder rename

---
+++ boxerclub-bg-rest-server/src/main/java/bg/boxerclub/boxerclubbgrestserver/BoxerclubBgRestServerApplication.java
@@ -0,0 +1,13 @@
+package bg.boxerclub.boxerclubbgrestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxerclubBgRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxerclubBgRestServerApplication.class, args);
+	}
+
+}
---
+++ boxerclub-bg-rest-server/src/test/java/bg/boxerclub/boxerclubbgrestserver/BoxerclubBgRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.boxerclub.boxerclubbgrestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxerclubBgRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
